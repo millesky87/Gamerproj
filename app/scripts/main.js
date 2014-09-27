284c917c14255c493f8041e53b4a5d19b11c50ec
MC3D <m.chapman.home@gmail.com>
2014-09-27 14:30:49 -0400

    adds player weapons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,10 +16,3 @@ Template.prototype.render = function(data){
 	var template = _.template($("#"+this.id).text());
 	$("."+this.where)[this.method](template(data));
 };
-
-
-$("#play").on('click', function(e){
-	$(".main-wrap").removeClass("hide");
-	$("#home").addClass("hide");
-	e.preventDefault();
-});
